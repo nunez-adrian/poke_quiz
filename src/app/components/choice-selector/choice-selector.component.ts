1cdda1abfb89444e69d500c95731bc12bf9592ec
Adrian Nuñez <nunezruizadrian@gmail.com>
2024-12-26 22:51:30 +0100

    docs: update doc

--- src/app/components/choice-selector/choice-selector.component.ts
+++ src/app/components/choice-selector/choice-selector.component.ts
@@ -21,21 +21,21 @@ export class ChoiceSelectorComponent implements OnInit{
     this.readPokemons();
   }
 
-  private pokemonData = inject(PokeapiService);
-  pokemon = signal<Pokemon | null>(null);
-  pokemons = signal<Pokemon[]>([]);
+  pokemonData = inject(PokeapiService); // Inyectamos el servicio de pokeapi
+  pokemon = signal<Pokemon | null>(null); // Aqui almacenamos al pokemon elegido
+  pokemons = signal<Pokemon[]>([]); // Aqui todos los que hemos leido
   error = signal(false);
   noChoice = false;
+  selectedPokemon: number | null = null; // Almacena el id del pokemon seleccionado por el usuario
 
-  // Almacena el pokemon seleccionado
-  selectedPokemon: number | null = null;
-
+  /**
+   * Lee los datos de 4 pokemons aleatorios y los almacena en la variable pokemons
+   */
   readPokemons() {
     const randomIds = Array.from({ length: 4 }, () => this.getRandomNumber(1, 151));
-    const pokemonObservables: Observable<Pokemon>[] = randomIds.map(id => this.pokemonData.readPokemons$(id)); // Aqui es donde almacenamos los observables. 1 por cada peticion que hemos hemos
+    const pokemonObservables: Observable<Pokemon>[] = randomIds.map(id => this.pokemonData.readPokemons$(id));
 
-    // Usamos forkJoin para esperar a que todas las solicitudes terminen y pasamos al signal pokemons los datos que hemos recogido en el pokemonObservables
-    // (Podriamos usar tambien Promise.all y funcionaria igual)
+    // Usamos forkJoin para esperar a que todas las solicitudes terminen y pasamos al signal los datos que hemos recogido en el pokemonObservables
     forkJoin(pokemonObservables).subscribe({
       next: (data: (Pokemon | null)[]) => {
         this.pokemons.set(data as Pokemon[]);
@@ -52,11 +52,12 @@ export class ChoiceSelectorComponent implements OnInit{
     });
   }
 
-  getRandomNumber(min: number, max: number) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
-  // Método que se llama cuando un checkbox cambia
+  /**
+   * Almacena al pokemon elegido en la variable para posteriormente compararlo
+   * 
+   * @param pokemon 
+   * @returns 
+   */
   onSelectPokemon(pokemon: number) {
     if (this.selectedPokemon === pokemon) {
       this.selectedPokemon = null;
@@ -66,14 +67,21 @@ export class ChoiceSelectorComponent implements OnInit{
     this.selectedPokemon = pokemon;
   }
 
+  /**
+   * Comprueba el resultado de la elección del usuario. Si el usuario acierta, vamos a la pantalla de victoria. Si no, a la de derrota
+   * Si no se elegió ningún pokemon, mostramos un error
+   * 
+   * @returns
+   */
   checkResult() {
     if (this.selectedPokemon === null) {
       this.noChoice = true;
 
-      // Opcional: Eliminar el error automáticamente después de la animación
+      // Elimina el error automáticamente después de la animación. el setTimeout dura lo mismo que la animacion
       setTimeout(() => {
         this.noChoice = false;
-      }, 400); // Duración igual a la animación en CSS
+      }, 400);
+
       return;
     }
 
@@ -82,5 +90,16 @@ export class ChoiceSelectorComponent implements OnInit{
     } else {
       // this.router.navigate(['/lose']);
     }
+  }
+
+  /**
+   * Recibe dos valores y devuelve un numero aleatorio entre esos dos valores
+   * 
+   * @param min 
+   * @param max 
+   * @returns number
+   */
+  getRandomNumber(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
   }
 }
